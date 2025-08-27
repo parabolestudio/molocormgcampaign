b47bc05cec982d3e1806c64f66a13ba00ea008f2
Kristin Baumann <hello@kristin-baumann.com>
2025-08-27 14:47:57 +0200

    Added tooltip for consumer trends

--- js/consumerTrends.js
+++ js/consumerTrends.js
@@ -5,6 +5,8 @@ import {
   buttonToVariableMapping,
   addMissingDaysPrev,
   addMissingDaysCurrent,
+  formatDate,
+  variableFormatting,
 } from "./helpers.js";
 import { getDropdownValue } from "./populateGeneralDropdowns.js";
 
@@ -14,6 +16,7 @@ export function ConsumerTrends() {
   const [country, setCountry] = useState(getDropdownValue("country"));
   const [field, setField] = useState(getDropdownValue("field"));
   const [data, setData] = useState([]);
+  const [hoveredItem, setHoveredItem] = useState(null);
 
   // listen to change in general system dropdown
   useEffect(() => {
@@ -191,92 +194,149 @@ export function ConsumerTrends() {
   const yAxisTicks = valueScale.ticks(4);
 
   // stroke-dasharray="0.10000000149011612, 10"
-  return html`<svg viewBox="0 0 ${width} ${height}">
-    <text dy="15" style="fill: orange; font-size: 12px;">
-      Debug: ${selectedVariable} | ${field} | ${system} | ${country} ||
-      #datapoints prev year: ${datapointsPrev.length} || #datapoints current
-      year: ${datapointsCurrent.length}
-    </text>
-    <g transform="translate(${margin.left}, ${margin.top})">
-      <line y2="${innerHeight}" stroke="black" />
-      <g class="y-axis">
-        ${yAxisTicks.map((tick) => {
-          return html`
-            <g transform="translate(0, ${valueScale(tick)})">
-              <text
-                x="${-10}"
-                y="${0}"
-                dominant-baseline="middle"
-                text-anchor="end"
-                class="charts-text-body"
-              >
-                ${tick >= 1_000_000
-                  ? (tick / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M"
-                  : tick >= 1_000
-                  ? (tick / 1_000).toFixed(1).replace(/\.0$/, "") + "k"
-                  : tick}
-              </text>
-            </g>
-          `;
-        })}
-      </g>
-      <g transform="translate(${axisOffsetX}, 0)">
-        <g class="x-axis">
-          ${xAxisTicks.map((tick, index) => {
-            if (index < xAxisTicks.length - 1) {
-              xAxisTicksWidth =
-                prevTime(xAxisTicks[index + 1]) - prevTime(xAxisTicks[index]);
-            }
+  return html`<div style="position: relative;">
+    <svg
+      viewBox="0 0 ${width} ${height}"
+      onmousemove="${(event) => {
+        const pointer = d3.pointer(event);
+
+        const leftSide = margin.left + axisOffsetX;
+        const rightSide = leftSide + chartWidth;
+
+        if (pointer[0] >= leftSide && pointer[0] <= rightSide) {
+          const innerX = pointer[0] - margin.left - axisOffsetX;
+          const date = prevTime.invert(innerX).toISOString().slice(0, 10);
+
+          setHoveredItem({
+            x: innerX,
+            date,
+          });
+        } else {
+          setHoveredItem(null);
+        }
+      }}"
+      onmouseleave="${() => setHoveredItem(null)}"
+    >
+      <text dy="15" style="fill: orange; font-size: 12px;">
+        Debug: ${selectedVariable} | ${field} | ${system} | ${country} ||
+        #datapoints prev year: ${datapointsPrev.length} || #datapoints current
+        year: ${datapointsCurrent.length}
+      </text>
+      <g transform="translate(${margin.left}, ${margin.top})">
+        <line y2="${innerHeight}" stroke="black" />
+        <g class="y-axis">
+          ${yAxisTicks.map((tick) => {
             return html`
-              <g transform="translate(${prevTime(tick)}, 0)">
-                <rect
-                  x="${0}"
-                  y="${0}"
-                  width="${xAxisTicksWidth}"
-                  height="${innerHeight}"
-                  fill="#D9D9D933"
-                  stroke="white"
-                  stroke-width="5"
-                />
-                <rect
-                  x="${0}"
-                  y="${innerHeight + 5}"
-                  width="${xAxisTicksWidth}"
-                  height="${40}"
-                  fill="#D3F5FF"
-                  stroke="white"
-                  stroke-width="5"
-                />
+              <g transform="translate(0, ${valueScale(tick)})">
                 <text
-                  x="${xAxisTicksWidth / 2}"
-                  y="${innerHeight + 5 + 20}"
+                  x="${-10}"
+                  y="${0}"
                   dominant-baseline="middle"
-                  text-anchor="middle"
+                  text-anchor="end"
                   class="charts-text-body"
                 >
-                  ${d3.timeFormat("%B")(tick)}
-                </text></g
-              >
+                  ${tick >= 1_000_000
+                    ? (tick / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M"
+                    : tick >= 1_000
+                    ? (tick / 1_000).toFixed(1).replace(/\.0$/, "") + "k"
+                    : tick}
+                </text>
+              </g>
             `;
           })}
         </g>
-        <path
-          d="${prevLine(addMissingDaysPrev(datapointsPrev))}"
-          fill="none"
-          stroke="#C3C3C3"
-          stroke-width="3"
-          stroke-joint="round"
-          stroke-linecap="round"
-          style="transition: all ease 0.3s"
-        />
-        <path
-          d="${currentLine(addMissingDaysCurrent(datapointsCurrent))}"
-          fill="none"
-          stroke="#0280FB"
-          stroke-width="3"
-          style="transition: all ease 0.3s"
-        />
+        <g transform="translate(${axisOffsetX}, 0)">
+          <g class="x-axis">
+            ${xAxisTicks.map((tick, index) => {
+              if (index < xAxisTicks.length - 1) {
+                xAxisTicksWidth =
+                  prevTime(xAxisTicks[index + 1]) - prevTime(xAxisTicks[index]);
+              }
+              return html`
+                <g transform="translate(${prevTime(tick)}, 0)">
+                  <rect
+                    x="${0}"
+                    y="${0}"
+                    width="${xAxisTicksWidth}"
+                    height="${innerHeight}"
+                    fill="#D9D9D933"
+                    stroke="white"
+                    stroke-width="5"
+                  />
+                  <rect
+                    x="${0}"
+                    y="${innerHeight + 5}"
+                    width="${xAxisTicksWidth}"
+                    height="${40}"
+                    fill="#D3F5FF"
+                    stroke="white"
+                    stroke-width="5"
+                  />
+                  <text
+                    x="${xAxisTicksWidth / 2}"
+                    y="${innerHeight + 5 + 20}"
+                    dominant-baseline="middle"
+                    text-anchor="middle"
+                    class="charts-text-body"
+                  >
+                    ${d3.timeFormat("%B")(tick)}
+                  </text></g
+                >
+              `;
+            })}
+          </g>
+
+          <path
+            d="${prevLine(addMissingDaysPrev(datapointsPrev))}"
+            fill="none"
+            stroke="#C3C3C3"
+            stroke-width="3"
+            stroke-joint="round"
+            stroke-linecap="round"
+            style="transition: all ease 0.3s"
+          />
+          <path
+            d="${currentLine(addMissingDaysCurrent(datapointsCurrent))}"
+            fill="none"
+            stroke="#0280FB"
+            stroke-width="3"
+            style="transition: all ease 0.3s"
+          />
+          ${hoveredItem
+            ? html`<line
+                x1="${hoveredItem.x}"
+                x2="${hoveredItem.x}"
+                y1="0"
+                y2="${innerHeight}"
+                stroke="black"
+                stroke-dasharray="2,2"
+              />`
+            : null}
+        </g>
       </g>
-    </g>
-  </svg>`;
+    </svg>
+    <${Tooltip} hoveredItem=${hoveredItem} />
+  </div> `;
 }
+
+function Tooltip({ hoveredItem }) {
+  if (!hoveredItem) return null;
+
+  return html`<div
+    class="tooltip"
+    style="left: ${hoveredItem.x}px; top: ${hoveredItem.y}px;"
+  >
+    <p class="tooltip-title">${formatDate(hoveredItem.date)}</p>
+  </div>`;
+}
+// <div>
+//   <p class="tooltip-label">${hoveredItem.variable}</p>
+//   <p class="tooltip-value">
+//     ${variableFormatting[buttonToVariableMapping[hoveredItem.variable]]
+//       ? variableFormatting[buttonToVariableMapping[hoveredItem.variable]](
+//           hoveredItem.value,
+//           2
+//         )
+//       : hoveredItem.value}
+//   </p>
+// </div>
