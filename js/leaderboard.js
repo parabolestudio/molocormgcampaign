bd68846f9287710b6395c0dea4e7b0e7b3712821
Kristin Baumann <hello@kristin-baumann.com>
2025-08-28 12:18:37 +0200

    Started optimizing leaderboard for mobile

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,6 +1,6 @@
 import { html, useState, useEffect } from "./utils/preact-htm.js";
 import { getDropdownValue } from "./populateGeneralDropdowns.js";
-import { variableFormatting } from "./helpers.js";
+import { variableFormatting, isMobile } from "./helpers.js";
 
 const INITIAL_VISIBLE = 5;
 const MAX_VISIBLE = 10;
@@ -82,38 +82,58 @@ export function Leaderboard() {
           : "#CCF5E8"}; border-radius: 10px;"
       ></div>
       <div
-        class="row-rank"
-        style="grid-area: ${d.rnk + 1} / 1 / ${d.rnk +
-        2} / 2; padding: 10px; background-opacity: 0.1;"
+        class="row-cell row-rank"
+        style="grid-area: ${d.rnk + 1} / 1 / ${d.rnk + 2} / 2; "
       >
+        ${isMobile
+          ? html`<span class="charts-text-body-bold row-title-mobile"
+              >Rank</span
+            >`
+          : null}
         ${d.rnk}.
       </div>
       <div
-        class="row-name"
-        style="grid-area: ${d.rnk + 1} / 2 / ${d.rnk +
-        2} / 3; padding: 10px; background-opacity: 0.1;"
+        class="row-cell row-name"
+        style="grid-area: ${d.rnk + 1} / 2 / ${d.rnk + 2} / 3;"
       >
+        ${isMobile
+          ? html`<span class="charts-text-body-bold row-title-mobile"
+              >Name</span
+            >`
+          : null}
         Advertiser ${d.rnk}
       </div>
       <div
-        class="row-value"
-        style="grid-area: ${d.rnk + 1} / 3 / ${d.rnk +
-        2} / 4; padding: 10px; background-opacity: 0.1;"
+        class="row-cell row-value"
+        style="grid-area: ${d.rnk + 1} / 3 / ${d.rnk + 2} / 4;"
       >
+        ${isMobile
+          ? html`<span class="charts-text-body-bold row-title-mobile"
+              >Avg CPM</span
+            >`
+          : null}
         ${variableFormatting["cpm"](d.cpm)}
       </div>
       <div
-        class="row-value"
-        style="grid-area: ${d.rnk + 1} / 4 / ${d.rnk +
-        2} / 5; padding: 10px; background-opacity: 0.1;"
+        class="row-cell row-value"
+        style="grid-area: ${d.rnk + 1} / 4 / ${d.rnk + 2} / 5;"
       >
+        ${isMobile
+          ? html`<span class="charts-text-body-bold row-title-mobile"
+              >Avg CPI</span
+            >`
+          : null}
         ${variableFormatting["cpi"](d.cpi)}
       </div>
       <div
-        class="row-value"
-        style="grid-area: ${d.rnk + 1} / 5 / ${d.rnk +
-        2} / 6; padding: 10px; background-opacity: 0.1;"
+        class="row-cell row-value"
+        style="grid-area: ${d.rnk + 1} / 5 / ${d.rnk + 2} / 6;"
       >
+        ${isMobile
+          ? html`<span class="charts-text-body-bold row-title-mobile"
+              >Avg FTD</span
+            >`
+          : null}
         ${d.ftd_rate !== 0 ? d.ftd_rate.toFixed(2) : ""}
       </div>
     `;
@@ -124,19 +144,27 @@ export function Leaderboard() {
     style="display: flex; flex-direction: column; align-items: flex-end;"
   >
     <div
-      style="display: grid; row-gap: 7px; grid-template-columns: 80px auto repeat(3, 150px); width: 100%;"
+      style="display: grid; row-gap: 7px; ${isMobile
+        ? "grid-template-columns: auto;"
+        : "grid-template-columns: 80px auto repeat(3, 150px);"} width: 100%;"
     >
-      <div class="header-cell" style="grid-area: 1 / 1 / 2 / 2;">Rank</div>
-      <div class="header-cell" style="grid-area: 1 / 2 / 2 / 3;">Name</div>
-      <div class="header-cell" style="grid-area: 1 / 3 / 2 / 4;">
-        Average CPM
-      </div>
-      <div class="header-cell" style="grid-area: 1 / 4 / 2 / 5;">
-        Average CPI
-      </div>
-      <div class="header-cell" style="grid-area: 1 / 5 / 2 / 6;">
-        Average FTD
-      </div>
+      ${!isMobile
+        ? html` <div class="header-cell" style="grid-area: 1 / 1 / 2 / 2;">
+              Rank
+            </div>
+            <div class="header-cell" style="grid-area: 1 / 2 / 2 / 3;">
+              Name
+            </div>
+            <div class="header-cell" style="grid-area: 1 / 3 / 2 / 4;">
+              Average CPM
+            </div>
+            <div class="header-cell" style="grid-area: 1 / 4 / 2 / 5;">
+              Average CPI
+            </div>
+            <div class="header-cell" style="grid-area: 1 / 5 / 2 / 6;">
+              Average FTD
+            </div>`
+        : null}
       ${rows}
     </div>
     ${filteredData.length > INITIAL_VISIBLE
