85a62275a2453f12bd2d3752528133bafd5a3720
Kristin Baumann <hello@kristin-baumann.com>
2025-08-28 12:27:45 +0200

    Changed styling grid in leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -76,96 +76,95 @@ export function Leaderboard() {
   const rows = visibleRows.map((d) => {
     return html`
       <div
-        style="grid-area: ${d.rnk + 1} / 1 / ${d.rnk +
-        2} / 6; background: ${d.rnk === 1
+        class="row"
+        style="display: grid;grid-template-columns: 80px auto repeat(3, 150px); width: 100%; border-radius: 10px; background: ${d.rnk ===
+        1
           ? "#60E2B7"
-          : "#CCF5E8"}; border-radius: 10px;"
-      ></div>
-      <div
-        class="row-cell row-rank"
-        style="grid-area: ${d.rnk + 1} / 1 / ${d.rnk + 2} / 2; "
+          : "#CCF5E8"};"
       >
-        ${isMobile
-          ? html`<span class="charts-text-body-bold row-title-mobile"
-              >Rank</span
-            >`
-          : null}
-        ${d.rnk}.
-      </div>
-      <div
-        class="row-cell row-name"
-        style="grid-area: ${d.rnk + 1} / 2 / ${d.rnk + 2} / 3;"
-      >
-        ${isMobile
-          ? html`<span class="charts-text-body-bold row-title-mobile"
-              >Name</span
-            >`
-          : null}
-        Advertiser ${d.rnk}
-      </div>
-      <div
-        class="row-cell row-value"
-        style="grid-area: ${d.rnk + 1} / 3 / ${d.rnk + 2} / 4;"
-      >
-        ${isMobile
-          ? html`<span class="charts-text-body-bold row-title-mobile"
-              >Avg CPM</span
-            >`
-          : null}
-        ${variableFormatting["cpm"](d.cpm)}
-      </div>
-      <div
-        class="row-cell row-value"
-        style="grid-area: ${d.rnk + 1} / 4 / ${d.rnk + 2} / 5;"
-      >
-        ${isMobile
-          ? html`<span class="charts-text-body-bold row-title-mobile"
-              >Avg CPI</span
-            >`
-          : null}
-        ${variableFormatting["cpi"](d.cpi)}
-      </div>
-      <div
-        class="row-cell row-value"
-        style="grid-area: ${d.rnk + 1} / 5 / ${d.rnk + 2} / 6;"
-      >
-        ${isMobile
-          ? html`<span class="charts-text-body-bold row-title-mobile"
-              >Avg FTD</span
-            >`
-          : null}
-        ${d.ftd_rate !== 0 ? d.ftd_rate.toFixed(2) : ""}
+        <div
+          class="row-cell row-rank"
+          style="grid-area: ${d.rnk + 1} / 1 / ${d.rnk + 2} / 2; "
+        >
+          ${isMobile
+            ? html`<span class="charts-text-body-bold row-title-mobile"
+                >Rank</span
+              >`
+            : null}
+          ${d.rnk}.
+        </div>
+        <div
+          class="row-cell row-name"
+          style="grid-area: ${d.rnk + 1} / 2 / ${d.rnk + 2} / 3;"
+        >
+          ${isMobile
+            ? html`<span class="charts-text-body-bold row-title-mobile"
+                >Name</span
+              >`
+            : null}
+          Advertiser ${d.rnk}
+        </div>
+        <div
+          class="row-cell row-value"
+          style="grid-area: ${d.rnk + 1} / 3 / ${d.rnk + 2} / 4;"
+        >
+          ${isMobile
+            ? html`<span class="charts-text-body-bold row-title-mobile"
+                >Avg CPM</span
+              >`
+            : null}
+          ${variableFormatting["cpm"](d.cpm)}
+        </div>
+        <div
+          class="row-cell row-value"
+          style="grid-area: ${d.rnk + 1} / 4 / ${d.rnk + 2} / 5;"
+        >
+          ${isMobile
+            ? html`<span class="charts-text-body-bold row-title-mobile"
+                >Avg CPI</span
+              >`
+            : null}
+          ${variableFormatting["cpi"](d.cpi)}
+        </div>
+        <div
+          class="row-cell row-value"
+          style="grid-area: ${d.rnk + 1} / 5 / ${d.rnk + 2} / 6;"
+        >
+          ${isMobile
+            ? html`<span class="charts-text-body-bold row-title-mobile"
+                >Avg FTD</span
+              >`
+            : null}
+          ${d.ftd_rate !== 0 ? d.ftd_rate.toFixed(2) : ""}
+        </div>
       </div>
     `;
   });
+
+  const headerRow = html`
+    <div
+      style="display: grid; grid-template-columns: 80px auto repeat(3, 150px); width: 100%;"
+    >
+      <div class="header-cell" style="grid-area: 1 / 1 / 2 / 2;">Rank</div>
+      <div class="header-cell" style="grid-area: 1 / 2 / 2 / 3;">Name</div>
+      <div class="header-cell" style="grid-area: 1 / 3 / 2 / 4;">
+        Average CPM
+      </div>
+      <div class="header-cell" style="grid-area: 1 / 4 / 2 / 5;">
+        Average CPI
+      </div>
+      <div class="header-cell" style="grid-area: 1 / 5 / 2 / 6;">
+        Average FTD
+      </div>
+    </div>
+  `;
 
   return html`<div
     id="leaderboard-container"
     style="display: flex; flex-direction: column; align-items: flex-end;"
   >
-    <div
-      style="display: grid; row-gap: 7px; ${isMobile
-        ? "grid-template-columns: auto;"
-        : "grid-template-columns: 80px auto repeat(3, 150px);"} width: 100%;"
-    >
-      ${!isMobile
-        ? html` <div class="header-cell" style="grid-area: 1 / 1 / 2 / 2;">
-              Rank
-            </div>
-            <div class="header-cell" style="grid-area: 1 / 2 / 2 / 3;">
-              Name
-            </div>
-            <div class="header-cell" style="grid-area: 1 / 3 / 2 / 4;">
-              Average CPM
-            </div>
-            <div class="header-cell" style="grid-area: 1 / 4 / 2 / 5;">
-              Average CPI
-            </div>
-            <div class="header-cell" style="grid-area: 1 / 5 / 2 / 6;">
-              Average FTD
-            </div>`
-        : null}
-      ${rows}
+    <div style="display: flex; flex-direction: column; gap: 8px;width: 100%;">
+      ${isMobile ? null : headerRow} ${rows}
     </div>
     ${filteredData.length > INITIAL_VISIBLE
       ? html`
@@ -180,3 +179,5 @@ export function Leaderboard() {
       : null}
   </div>`;
 }
+
+// ${!isMobile ? headerCells : null} ${rows}
