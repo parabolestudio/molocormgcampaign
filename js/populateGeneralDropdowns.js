f0b22599ddf2d54612103a1e9026115af42ae007
Kristin Baumann <hello@kristin-baumann.com>
2025-10-01 13:44:38 +0200

    Adjusted for UK

--- js/populateGeneralDropdowns.js
+++ js/populateGeneralDropdowns.js
@@ -5,17 +5,6 @@ export function populateGeneralDropdowns() {
   let countryDropdown = document.querySelector("#vis-general-dropdown-country");
   let systemDropdown = document.querySelector("#vis-general-dropdown-system");
 
-  const fields = [
-    {
-      text: "Sportsbetting",
-      value: "Sportsbetting",
-    },
-    {
-      text: "Gambling & Casino",
-      value: "RMG",
-    },
-  ];
-  const fieldDefault = fields[0];
   const countries = [
     {
       text: "U.S.",
@@ -30,7 +19,39 @@ export function populateGeneralDropdowns() {
     "testCountry"
   );
   console.log("testCountry param:", countryParam);
-  const countryDefault = countryParam === "GBR" ? countries[1] : countries[0];
+  const countryDefault =
+    countryParam && countryParam === "GBR" ? countries[1] : countries[0];
+
+  const fields = [
+    {
+      text: "Sportsbetting",
+      value: "Sportsbetting",
+    },
+    {
+      text: "Gambling & Casino",
+      value: "RMG",
+    },
+  ];
+  const fieldsGBR = [
+    {
+      text: "Gambling & Casino",
+      value: "RMG",
+    },
+  ];
+
+  function getFieldsSet() {
+    if (countryParam === "GBR") {
+      return fieldsGBR;
+    }
+    const fieldsSet =
+      getDropdownValue("country") === "GBR" ? fieldsGBR : fields;
+    return fieldsSet;
+  }
+
+  function getFieldDefault() {
+    return getFieldsSet()[0];
+  }
+
   const systems = [
     {
       text: "iOS",
@@ -43,23 +64,31 @@ export function populateGeneralDropdowns() {
   ];
   const systemDefault = systems[0];
 
-  if (fieldDropdown) {
-    if (fieldDropdown) fieldDropdown.innerHTML = "";
-    fields.forEach((field) => {
-      let option = document.createElement("option");
-      option.value = field.value;
-      option.text = field.text;
-      fieldDropdown.add(option);
-    });
-    fieldDropdown.value = fieldDefault.value;
-    fieldDropdown.addEventListener("change", (e) => {
-      // Dispatch custom event to notify other components
-      document.dispatchEvent(
-        new CustomEvent("vis-general-dropdown-field-changed", {
-          detail: { selectedField: e.target.value },
-        })
-      );
-    });
+  populateFieldDropdown();
+  populateSystemDropdown();
+  showHideUsOnlySections();
+
+  function populateFieldDropdown() {
+    const fieldsSet = getFieldsSet();
+    if (fieldDropdown) {
+      if (fieldDropdown) fieldDropdown.innerHTML = "";
+      fieldsSet.forEach((field) => {
+        let option = document.createElement("option");
+        option.value = field.value;
+        option.text = field.text;
+        fieldDropdown.add(option);
+      });
+      fieldDropdown.value = getFieldDefault().value;
+      fieldDropdown.disabled = fieldsSet.length === 1;
+      fieldDropdown.addEventListener("change", (e) => {
+        // Dispatch custom event to notify other components
+        document.dispatchEvent(
+          new CustomEvent("vis-general-dropdown-field-changed", {
+            detail: { selectedField: e.target.value },
+          })
+        );
+      });
+    }
   }
 
   if (countryDropdown) {
@@ -80,26 +109,35 @@ export function populateGeneralDropdowns() {
           detail: { selectedCountry: e.target.value },
         })
       );
+      populateFieldDropdown();
+      populateSystemDropdown();
+
+      showHideUsOnlySections(
+        getDropdownValue("country") === "GBR" ? false : true
+      );
     });
   }
 
-  if (systemDropdown) {
-    if (systemDropdown) systemDropdown.innerHTML = "";
-    systems.forEach((system) => {
-      let option = document.createElement("option");
-      option.text = system.text;
-      option.value = system.value;
-      systemDropdown.add(option);
-    });
-    systemDropdown.value = systemDefault.value;
-    systemDropdown.addEventListener("change", (e) => {
-      // Dispatch custom event to notify other components
-      document.dispatchEvent(
-        new CustomEvent("vis-general-dropdown-system-changed", {
-          detail: { selectedSystem: e.target.value },
-        })
-      );
-    });
+  function populateSystemDropdown() {
+    if (systemDropdown) {
+      if (systemDropdown) systemDropdown.innerHTML = "";
+      systems.forEach((system) => {
+        let option = document.createElement("option");
+        option.text = system.text;
+        option.value = system.value;
+        systemDropdown.add(option);
+      });
+      systemDropdown.value = systemDefault.value;
+      systemDropdown.disabled = getDropdownValue("country") === "GBR";
+      systemDropdown.addEventListener("change", (e) => {
+        // Dispatch custom event to notify other components
+        document.dispatchEvent(
+          new CustomEvent("vis-general-dropdown-system-changed", {
+            detail: { selectedSystem: e.target.value },
+          })
+        );
+      });
+    }
   }
 }
 
@@ -107,4 +145,11 @@ export function getDropdownValue(setting = "system") {
   // find out selected value of dropdown with id vis-general-dropdown-system
   const dropdown = document.querySelector(`#vis-general-dropdown-${setting}`);
   return dropdown ? dropdown.value : null;
+}
+
+function showHideUsOnlySections(show = true) {
+  const sections = document.getElementsByClassName("us-only-section");
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.display = show ? "block" : "none";
+  }
 }
